0efdd4e04fa6ff762c9cd4c2939f47a41f892027
shaloojais8876 <104994730+shaloojais8876@users.noreply.github.com>
2024-07-17 14:42:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -17,13 +17,13 @@ class Person {
 
 class Student extends Person {
 	study(){
-		console.log(`${this._name} is studying`);
+		console.log(`${this.name} is studying`);
 	}
 }
 
 class Teacher extends Person {
 	teach(){
-		console.log(`${this._name} is teaching`);
+		console.log(`${this.name} is teaching`);
 	}
 }
 const person = new Person("John", 25);
